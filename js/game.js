e83ab165940ce94299b50c2cb220c2577608395b
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-10-14 16:44:10 +0200

    checking

--- js/game.js
+++ js/game.js
@@ -2,3 +2,4 @@ const canvas = document.querySelector('canvas')
 canvas.style.border = '5px solid black'
 const ctx = canvas.getContext('2d')
 const startScreen = document.querySelector('.game-intro')
+//
