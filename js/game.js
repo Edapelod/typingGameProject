d72d8412c01d108ee08090cafced2c7facf3d01d
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-10-17 17:48:45 +0200

    square repeating

--- js/game.js
+++ js/game.js
@@ -1,5 +1,46 @@
 const canvas = document.querySelector('canvas')
-canvas.style.border = '5px solid black'
+canvas.style.border = '3px solid black'
 const ctx = canvas.getContext('2d')
 const startScreen = document.querySelector('.game-intro')
-//
+
+// variables
+const background = new Image()
+background.src = '../Img/background1.jpg'
+
+let div = document.createElement("div")
+let squareY = -70
+let squareX = Math.random() * (canvas.width - 300)
+let isGameOver = false
+let gameId = 0
+const randomQuote = 0
+
+const drawSquare = () => {
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.fillRect(squareX, squareY, 350, 70);
+    ctx.closePath();
+}
+//window onload
+window.onload = () => {
+    document.getElementById("start-button").onclick = () => {
+      console.log("starting");
+      //song.play();
+      startGame();
+    }}
+//recursive function
+function startGame () {
+    startScreen.style.display = "none";
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    drawSquare();
+    squareY += 5.5
+    if (squareY > canvas.height) {
+        squareY = -70;
+        squareX = Math.random() * (canvas.width - 200)
+    }
+    if (isGameOver) {
+        cancelAnimationFrame(gameId)
+      } else {
+        // Ask for a new frame
+        gameId = requestAnimationFrame(startGame)
+      }
+}
