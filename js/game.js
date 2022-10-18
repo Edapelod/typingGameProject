f0f88abba743deb9431d1acc8801feff4a06b1eb
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-10-18 14:25:22 +0200

    some functionalities done

--- js/game.js
+++ js/game.js
@@ -6,19 +6,21 @@ const startScreen = document.querySelector('.game-intro')
 // variables
 const background = new Image()
 background.src = '../Img/background1.jpg'
-
-let div = document.createElement("div")
 let squareY = -70
 let squareX = Math.random() * (canvas.width - 300)
 let isGameOver = false
 let gameId = 0
-const randomQuote = 0
+let randomQuote = "hola"
+let newRandomQuotes = ["h","o","l","a","hhh"]
 
 const drawSquare = () => {
     ctx.beginPath();
     ctx.fillStyle = "white";
-    ctx.fillRect(squareX, squareY, 350, 70);
+    //ctx.fillRect(squareX, squareY, 350, 70);
+    ctx.font = '30px Arial';
+    ctx.fillText(randomQuote, squareX, squareY, 1350, 1570)
     ctx.closePath();
+    
 }
 //window onload
 window.onload = () => {
@@ -32,10 +34,12 @@ function startGame () {
     startScreen.style.display = "none";
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawSquare();
-    squareY += 5.5
+    squareY += 1.5
     if (squareY > canvas.height) {
         squareY = -70;
-        squareX = Math.random() * (canvas.width - 200)
+        squareX = Math.random() * (canvas.width - 200);
+        let nextQuote = newRandomQuotes[Math.floor(Math.random() * newRandomQuotes.length)]
+        randomQuote = nextQuote
     }
     if (isGameOver) {
         cancelAnimationFrame(gameId)
@@ -43,4 +47,14 @@ function startGame () {
         // Ask for a new frame
         gameId = requestAnimationFrame(startGame)
       }
+}
+const theInput = document.getElementById("typeHere")
+document.getElementById("typeHere").onkeyup = (e) => {
+
+console.log(theInput.value)
+    //console.log(e.target.value)
+    if (randomQuote === e.target.value) {
+        console.log("yes");
+        theInput.value = ""
+    }
 }
